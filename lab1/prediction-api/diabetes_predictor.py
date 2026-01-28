4f37249880dbdf77c24006e599fb98cc02eb644c
Indika Kumara <ipkumarawd@yahoo.com>
2026-01-28 12:06:56 +0100

    Updated Lab 1 APIs

--- lab1/prediction-api/diabetes_predictor.py
+++ lab1/prediction-api/diabetes_predictor.py
@@ -3,30 +3,36 @@ import os
 
 import pandas as pd
 from flask import jsonify
+from google.cloud import storage
 from keras.models import load_model
-import logging
-from io import StringIO
 
 
 class DiabetesPredictor:
     def __init__(self):
         self.model = None
 
-    def predict_single_record(self, prediction_input):
-        logging.debug(prediction_input)
-        if self.model is None:
-            try:
-                model_repo = os.environ['MODEL_REPO']
-                file_path = os.path.join(model_repo, "model.keras")
-                self.model = load_model(file_path)
-            except KeyError:
-                print("MODEL_REPO is undefined")
-                self.model = load_model('model.keras')
+    # download the model
+    def download_model(self):
+        project_id = os.environ.get('PROJECT_ID', 'Specified environment variable is not set.')
+        model_repo = os.environ.get('MODEL_REPO', 'Specified environment variable is not set.')
+        model_name = os.environ.get('MODEL_NAME', 'Specified environment variable is not set.')
+        client = storage.Client(project=project_id)
+        bucket = client.get_bucket(model_repo)
+        blob = bucket.blob(model_name)
+        blob.download_to_filename('local_model.h5')
+        self.model = load_model('local_model.h5')
+        return jsonify({'message': " the model was downloaded"}), 200
 
-        df = pd.read_json(StringIO(json.dumps(prediction_input)), orient='records')
+    def predict_single_record(self, prediction_input):
+        print(prediction_input)
+        if self.model is None:
+            self.download_model()
+        print(json.dumps(prediction_input))
+        df = pd.read_json(json.dumps(prediction_input), orient='records')
+        print(df)
         y_pred = self.model.predict(df)
-        logging.info(y_pred[0])
+        print(y_pred[0])
         status = (y_pred[0] > 0.5)
-        logging.info(type(status[0]))
+        print(type(status[0]))
         # return the prediction outcome as a json message. 200 is HTTP status code 200, indicating successful completion
         return jsonify({'result': str(status[0])}), 200
--- lab1/training-api/resources/model_trainer.py
+++ lab1/training-api/resources/model_trainer.py
@@ -6,9 +6,13 @@ import os
 from flask import jsonify
 from keras.layers import Dense
 from keras.models import Sequential
+from google.cloud import storage
 
 
 def train(dataset):
+    project_id = os.environ.get('PROJECT_ID', 'Specified environment variable is not set.')
+    model_repo = os.environ.get('MODEL_REPO', 'Specified environment variable is not set.')
+    model_name = os.environ.get('MODEL_NAME', 'Specified environment variable is not set.')
     # split into input (X) and output (Y) variables
     X = dataset[:, 0:8]
     Y = dataset[:, 8]
@@ -28,13 +32,16 @@ def train(dataset):
         "loss": scores[0],
     }
     logging.info(text_out)
+
+    model_path = os.path.join('/tmp', model_name)
+    model.save(model_path, save_format='h5')
+    # Save to GCS
+    client = storage.Client(project=project_id)
+    bucket = client.get_bucket(model_repo)
+    blob = bucket.blob(model_name)
+    blob.upload_from_filename(model_path)
+    # Do clean up
+    os.remove(model_path)
+
     # Saving model in a given location provided as an env. variable
-    model_repo = os.getenv('MODEL_REPO')
-    if model_repo:
-        file_path = os.path.join(model_repo, "model.keras")
-        model.save(file_path)
-        logging.info("Saved the model to the location : " + model_repo)
-        return jsonify(text_out), 200
-    else:
-        model.save("model.keras")
-        return jsonify({'message': 'The model was saved locally.'}), 200
+    return jsonify(text_out), 200
